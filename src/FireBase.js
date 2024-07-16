48ebcedcb6a77aa55f77b1b26c5e033481009a0c
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2024-07-16 10:20:22 +0530

    Feew Changes

--- src/FireBase.js
+++ src/FireBase.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOJyIWmso4qMU-6Rt1u_2b4D2J9iIuUaY",
-  authDomain: "e-commerce-2d17f.firebaseapp.com",
-  projectId: "e-commerce-2d17f",
-  storageBucket: "e-commerce-2d17f.appspot.com",
-  messagingSenderId: "104695489432",
-  appId: "1:104695489432:web:0ae09d7643fdb1625801ed"
+ 
 };
 
 // Initialize Firebase
